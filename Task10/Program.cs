9472a5a2a7272926b9c389d888024911f63bcc52
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-10 01:31:53 +0500

    task 10 is done

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Please enter 3 digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ShowMidNum(int number)
+{
+    int remain = number % 100;
+    return remain / 10;
+}
+
+Console.WriteLine(ShowMidNum(num));
