230eedae0c71ef06731a795b50af96d0fe2fa94f
Sanj Akhmedov <1999.sanjar.06@gmail.com>
2022-08-11 06:02:32 +0500

    task 15 is done, task 13 is in process

---
+++ Task13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет. Решить без использования строк.
+
+// Console.Write("Please enter the number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int index = num;
+// int result = 0;
+// // int find = 0;
+
+// if (num / 100 == 0)
+// {
+//     Console.WriteLine("There is no 3rd number!!!");
+// } 
+// else
+// {
+//     while (index / 100 == 0)
+//     {
+        
+//         result = num / 10;
+//         Console.WriteLine(result);
+//     }
+//     Console.WriteLine(result % 10);
+// }
+
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+int result = num;
+int div = 0;
+int index = 100;
+while (index < 10)
+{
+    result = num / div;
+    Console.WriteLine(index + ": " + result);
+    index++;
+    num += 100;
+    
+}
+Console.WriteLine(result);
---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую 
+// день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Please enter the number from 1 to 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+bool CheckDay(int num)
+{
+    return num == 6 || num == 7;
+}
+
+Console.WriteLine(CheckDay(day));
